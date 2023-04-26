3fc095d586251e22d2169c78e992a32e54032531
Ben Fleming <bfleming231@gmail.com>
2023-04-25 22:35:39 -0400

    Merge motivationId table into main branch

--- table_setup.py
+++ table_setup.py
@@ -53,7 +53,7 @@ conn.execute('''CREATE TABLE users
 # Need to create few lines to see if content.db is empty
 # If it isn't empty clear content.db
 # may write .sh file to do it if not easy in python
-print ("Table created successfully")
+print ("Users table created successfully")
 
 conn.execute('''CREATE TABLE resellers 
         (id String, 
@@ -68,6 +68,26 @@ conn.execute('''CREATE TABLE resellers
 # Need to create few lines to see if content.db is empty
 # If it isn't empty clear content.db
 # may write .sh file to do it if not easy in python
-print ("Table created successfully")
+print ("Resellers created successfully")
 
+conn.execute('''CREATE TABLE motivationId
+        (id varchar(50) PRIMARY KEY,
+        name varchar(50),
+        shortDescription varchar(50),
+        longDescription varchar(50),
+        longDescriptionPlural varchar(10),
+        additionalData varchar(50),
+        imageUrl varchar(50),
+        color varchar(50),
+        tailResolution varchar(50),
+        insights text[]);''')
+
+# Persons of interest and motivations need to be array
+# will create new type object for them later
+# Need to create few lines to see if content.db is empty
+# If it isn't empty clear content.db
+# may write .sh file to do it if not easy in python
+print ("MotivationId Table created successfully")
+
+conn.close()
 conn.close()
