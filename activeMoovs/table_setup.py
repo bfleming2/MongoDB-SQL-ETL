707f49742b003b3941a32f07df5685e52c3a31c8
Maya Pollack <mayapollack@MacBook-Pro-78.local>
2023-05-03 17:37:07 -0400

    activeMoovs table

--- activeMoovs/table_setup.py
+++ activeMoovs/table_setup.py
@@ -3,7 +3,7 @@ import os
 
 # Joining the file path for the content.db together to see if the file exists
 # Change directory to where your mongodb-sql-etl is located
-directory = "C:/Users/Ben Fleming/Desktop/TAMID/mongodb-sql-etl"
+directory = "/Users/mayapollack/Documents/TAMID/Claro"
 file = "content.db"
 file_path = os.path.join(directory, file)
 
@@ -16,15 +16,32 @@ with open(file_path, 'w') as f:
 # Opening a connection so we can execute a query on the content.db
 conn = sqlite3.connect("content.db")
 
-conn.execute('''CREATE TABLE activeMoovs 
+
+conn.execute('''CREATE TABLE activeMoovs
         (id varchar(50) PRIMARY KEY,
-         userID varchar(50), 
-         counterPartId varchar(50),
-         startDate varchar(50),
-         priority DOUBLE(5),
-         FOREIGN KEY(userID) REFERENCES users(id)),
-         FOREIGN KEY(moovID) REFERENCES moovs(id),
-         FOREIGN KEY(counterPart) REFERENCES ;''')
+        userID varchar(50), 
+        counterPartId varchar(50),
+        moovId varchar(50),
+        priority DOUBLE(5),
+        startDate varchar(50),
+        endDate varchar(50),
+        plannedEndDate varchar(50),
+        isOverdue BOOL,
+        notifiedUserForOverdue BOOL,
+        feedbackScore INTEGER(50), 
+        feedbackText varchar(50))
+;''')
+
+# FOREIGN KEY(userID) REFERENCES users(id)),
+# FOREIGN KEY(moovID) REFERENCES moovs(id),
+# FOREIGN KEY(counterPart) REFERENCES 
+         
+         
+
+# We removed events and steps arrays 
+# We'll figure out foreign keys after
+
+
 # Persons of interest and motivations need to be array
 # will create new type object for them later
 # Need to create few lines to see if content.db is empty
