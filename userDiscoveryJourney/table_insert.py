c06920bb95f1a0fb7b92ee594426d3918b6b956b
Ben Fleming <bfleming231@gmail.com>
2023-05-10 10:01:21 -0400

    New style of data sifting adding

--- userDiscoveryJourney/table_insert.py
+++ userDiscoveryJourney/table_insert.py
@@ -11,23 +11,17 @@ import os
 
 directory = "C:/Users/Ben Fleming/Desktop/TAMID/mongodb-sql-etl/"
 directory += "External_data"
-file = "motivations.json"
+file = "userDiscoveryJourney.json"
 file_path = os.path.join(directory, file)
+data = {}
 with open(file_path, 'rb') as f:
-    data = f.read()
-file = data.decode('utf-8').splitlines()
-lines = len(file)
-
-# Sets the file path for the users_clean.json file
-file = "motivations_clean.json"
-file_path = os.path.join(directory, file)
-
-# Checking to see if the file users_clean.json exists so it isn't remade
-if not os.path.exists(file_path):
-    # Cleans the files
-    # Change for your files
-    df = pd.read_json('External_data/motivations.json', lines=True)
-    df.to_json('External_data/motivations_clean.json')
+    try:
+        data = json.load(f)
+        print(data)
+    except ValueError as e:
+        for line in f:
+            data = json.load(line)
+            print(data)
 
 # Clearing the content.db database and then restarting the connection
 conn = sqlite3.connect("content.db")
@@ -50,31 +44,30 @@ for key in data:
 query = query[0:len(query) - 2] + ") \nVALUES"
 
 # Access each user in the users table with the outer for loop
-for index in range(lines):
-    sql_code = "("
-    # Iterates through the json (NEED TO DO THIS)
-    for key in data :
-        if str(key) == '_id' :
-            continue
+sql_code = "("
+# Iterates through the json (NEED TO DO THIS)
+for key in data :
+    if str(key) == '_id' :
+        continue
+    else :
+        if data[key][str(index)] == None or str(data[key][str(index)]) == "NULL" or data[key][str(index)] == "None" :
+            sql_code += "NULL, "
         else :
-            if data[key][str(index)] == None or str(data[key][str(index)]) == "NULL" or data[key][str(index)] == "None" :
-                sql_code += "NULL, "
+            if str(key) == "insights" :
+                sql_code += "\'{"
+                for item in data[key][str(index)]:
+                    sql_code += "\"" + str(item) + "\"" + ", "
+                if len(data[key][str(index)]) != 0 :
+                    sql_code = sql_code[0:len(sql_code) - 2]
+                sql_code += "}\'"
             else :
-                if str(key) == "insights" :
-                    sql_code += "\'{"
-                    for item in data[key][str(index)]:
-                        sql_code += "\"" + str(item) + "\"" + ", "
-                    if len(data[key][str(index)]) != 0 :
-                        sql_code = sql_code[0:len(sql_code) - 2]
-                    sql_code += "}\'"
-                else :
-                    sql_code += "\'" + str(data[key][str(index)]) + "\'"
-            sql_code += ", "
-    sql_code = sql_code[0:len(sql_code) - 2] + ")"
-    
-    if index != lines - 1 :
-        sql_code += ",\n"
-    query += sql_code 
+                sql_code += "\'" + str(data[key][str(index)]) + "\'"
+        sql_code += ", "
+sql_code = sql_code[0:len(sql_code) - 2] + ")"
+
+if index != lines - 1 :
+    sql_code += ",\n"
+query += sql_code 
 query += ";"
 conn.execute(query)
 conn.commit()
